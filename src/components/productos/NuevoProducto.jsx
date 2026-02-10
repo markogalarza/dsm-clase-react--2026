710fe65726b881dd6bc1f7d93008bd1e315058d0
unknown <marko.galarza@iee-por-71991.iee.a.unavarra.es>
2026-02-10 15:32:04 +0100

    React portals

--- src/components/productos/NuevoProducto.jsx
+++ src/components/productos/NuevoProducto.jsx
@@ -1,21 +1,53 @@
 import { useEffect, useRef, useState } from "react"
 import { Button, Form } from "react-bootstrap"
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import ReactDOM from 'react-dom'
+import './nuevoProducto.css'
+
+function InfoModal(props) {
+
+    const [open, setOpen] = useState(true)
+
+    const cerrarHandler = () => {
+        setOpen(false)
+    }
+    if (open) {
+        return (
+            <>
+                <h2>{props.titulo}</h2>
+                <p>{props.mensaje}</p>
+                <button onClick={cerrarHandler}>CERRAR</button>
+            </>
+        )
+    }
+
+    return null
+
+}
 
 function NuevoProducto(props) {
 
     useEffect(() => {
         refNombre.current.focus()
     }, [])
-    
+
     const [nombre, setNombre] = useState('')
     const [precio, setPrecio] = useState('')
     const [fecha, setFecha] = useState('')
 
+    const [nombreValid, setNombreValid] = useState(true)
+
     const nombreHandler = (event) => {
         setNombre(event.target.value)
+        setNombreValid(true)
+    }
+
+    const nombreBlurHandler = () => {
+        if (nombre.length === 0) {
+            setNombreValid(false)
+        }
     }
 
     const precioHandler = (event) => {
@@ -45,28 +77,37 @@ function NuevoProducto(props) {
         refNombre.current.focus()
     }
 
+    // const contenidoModal = <InfoModal titulo='Valida el formulario' mensaje='El campo nombre está vacío' />
+    const contenidoModal = ReactDOM.createPortal(
+        <InfoModal titulo='Valida el formulario' mensaje='El campo nombre está vacío' />
+        ,
+        document.getElementById('overlay'))
+
     return (
-        <Form onSubmit={submitHandler}>
-            <Container>
-                <Row>
-                    <Col>
-                        <Form.Label>Nombre:</Form.Label>
-                        <Form.Control ref={refNombre} type='text' onChange={nombreHandler} value={nombre} />
-                    </Col>
-                    <Col>
-                        <Form.Label>Precio:</Form.Label>
-                        <Form.Control type='number' onChange={precioHandler} value={precio} />
-                    </Col>
-                    <Col>
-                        <Form.Label>Fecha:</Form.Label>
-                        <Form.Control type='date' onChange={fechaHandler} value={fecha} />
-                    </Col>
-                    <Col>
-                        <Button variant='success' type='submit'>NUEVO PRODUCTO</Button>
-                    </Col>
-                </Row>
-            </Container>
-        </Form>
+        <>
+            {!nombreValid && contenidoModal}
+            <Form onSubmit={submitHandler}>
+                <Container>
+                    <Row>
+                        <Col>
+                            <Form.Label className={nombreValid ? '' : 'invalid'}>Nombre:</Form.Label>
+                            <Form.Control ref={refNombre} type='text' onChange={nombreHandler} onBlur={nombreBlurHandler} value={nombre} />
+                        </Col>
+                        <Col>
+                            <Form.Label>Precio:</Form.Label>
+                            <Form.Control type='number' onChange={precioHandler} value={precio} />
+                        </Col>
+                        <Col>
+                            <Form.Label>Fecha:</Form.Label>
+                            <Form.Control type='date' onChange={fechaHandler} value={fecha} />
+                        </Col>
+                        <Col>
+                            <Button variant='success' type='submit'>NUEVO PRODUCTO</Button>
+                        </Col>
+                    </Row>
+                </Container>
+            </Form>
+        </>
     )
 }
 
